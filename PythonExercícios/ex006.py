62b23161a78f832fbe03fe86471ad62286c57a66
erik-nathan <eriknathan.contato@gmail.com>
2022-01-10 20:15:37 -0300

    Exercises 5, 6 e 7

--- PythonExercícios/ex006.py
+++ PythonExercícios/ex006.py
@@ -1,1 +1,12 @@
 # CRIE UM ALGORITMO QUE LEIA UM NÚMERO E MOSTE O SEU DOBRO, TRIPLO E RAIZ QUADRADA
+
+import math
+print('============= DESAFIO 06 ==============')
+
+num = int(input('Informe um número: '))
+
+d = num * 2
+t = num * 3
+r = math.sqrt(num)
+
+print('O dobro do número {} é {}, o triplo é {} e a raiz quadrada é {}!'.format(num, d, t, r))
