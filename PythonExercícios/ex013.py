456a535fca97b91045432174bc413a479648c0a6
erik-nathan <eriknathan.contato@gmail.com>
2022-01-11 20:16:24 -0300

    Adicionando os exercícios do 8 ao 13

--- PythonExercícios/ex013.py
+++ PythonExercícios/ex013.py
@@ -1,1 +1,11 @@
-# FAÇA UM ALGORITMO QUE LEIA O SALÁRIO DE UM FUNCIONÁRIO E MOSTRE SEU NOVO SALÁRIO, COM 15% DE AUMENTO.
+# FAÇA UM ALGORITMO QUE LEIA O SALÁRIO DE UM FUNCIONÁRIO E MOSTRE SEU NOVO SALÁRIO,
+# COM 15% DE AUMENTO.
+
+print('============= DESAFIO 13 ==============')
+
+s = float(input('Informe o salário: '))
+
+newWage = (s * 15) / 100
+aumento = s + newWage
+
+print('O novo salário com aumento de 15% é: {}'.format(aumento))
