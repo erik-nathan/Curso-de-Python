6bf74913dbddd914b8db79918baedc1b41f0c843
erik-nathan <eriknathan.contato@gmail.com>
2022-01-27 17:00:24 -0300

    Adding Challenges 24

--- PythonExercícios/ex024.py
+++ PythonExercícios/ex024.py
@@ -1,3 +1,7 @@
-#  Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
+#  Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
 
 print('============= DESAFIO 24 - Verificando as primeiras letras de um texto ==============')
+
+cid = str(input('Em que cidade você nasceu: ')).strip()
+
+print(cid[:5].upper() == 'SANTO')
