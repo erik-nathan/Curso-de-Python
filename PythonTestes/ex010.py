ab556e04b1a405e8abb0ba04bec615125676de01
erik-nathan <eriknathan.contato@gmail.com>
2022-02-08 14:30:53 -0300

    Adding lesson 12

--- PythonTestes/ex010.py
+++ PythonTestes/ex010.py
@@ -9,8 +9,8 @@ else:
 
 
 # nome = str(input('Qual é o seu nome? '))
-# if nome == 'Erik':
-#     print('Que nome lindo você tem!')
-# else:
-#     print('Seu nome é tão normal!')
-# print('Bom dia, {}!'.format(nome))
+# # if nome == 'Erik':
+# #     print('Que nome lindo você tem!')
+# # else:
+# #     print('Seu nome é tão normal!')
+# # print('Bom dia, {}!'.format(nome))
