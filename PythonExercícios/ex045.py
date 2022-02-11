1a35efae8487c8a46fbde1c3a5148de55745268f
erik-nathan <eriknathan.contato@gmail.com>
2022-02-11 20:27:06 -0300

    Adding lesson 13

--- PythonExercícios/ex045.py
+++ PythonExercícios/ex045.py
@@ -3,14 +3,15 @@
 print('============= DESAFIO 45 - GAME: Pedra Papel e Tesoura ==============')
 from random import choice
 print('[PEDRA] | [PAPEL] | [TESOURA]')
+print('--'*24)
 aux = str(input('Escolha uma opção: '))
 jokenpo = choice(['pedra', 'papel', 'tesoura'])
 print('Máquina: ',jokenpo)
 print('=='*24)
 
 if aux == 'pedra' and jokenpo == 'tesoura' or aux == 'papel' and jokenpo == 'pedra' or aux == 'tesoura' and jokenpo == 'papel':
-    print('VOCÊ GANHOU')
+    print('VOCÊ GANHOU!!!')
 elif aux == 'pedra' and jokenpo == 'pedra' or aux == 'papel' and jokenpo == 'papel' or aux == 'tesoura' and jokenpo == 'tesoura':
-    print('EMPATOU')
+    print('EMPATOU!!!')
 else:
-    print('VOCÊ PERDEU')
+    print('VOCÊ PERDEU!!!')
