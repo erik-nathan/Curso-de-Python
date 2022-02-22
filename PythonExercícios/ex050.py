63dea35314b780925b368f00e98e3eaaae8cd324
erik-nathan <eriknathan.contato@gmail.com>
2022-02-22 15:14:02 -0300

    Adding Challeng 50

--- PythonExercícios/ex050.py
+++ PythonExercícios/ex050.py
@@ -2,4 +2,11 @@
 Se o valor digitado for ímpar, desconsidere-o.'''
 
 print('============= DESAFIO 50 - Soma dos pares ==============')
-
+soma = 0
+cont = 0
+for c in range(1, 7):
+    num = int(input('Digite {} valor: '.format(c)))
+    if num % 2 == 0:
+        soma = soma + num
+        cont = cont + 1
+print('Você informou {} números pares e a soma foi {}'.format(cont, soma))
