9c579501fd1e2ec4292ceabfb7225609cb1789fe
erik-nathan <eriknathan.contato@gmail.com>
2022-03-13 14:44:38 -0300

    Adding Class 16

---
+++ PythonTestes/aula16.py
@@ -0,0 +1,24 @@
+# TUPLAS SÃO IMUTÁVEIS
+
+pessoa = ('Erik', 39, 'M', 99.880)
+# del(pessoa)
+print(pessoa)
+
+# a = 2, 5, 4
+# b = 5, 8, 1, 2
+# c = a + b
+# print(c)
+# print(c.index(2))
+
+# lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita'
+# print(sorted(lanche))
+
+# for comida in lanche:
+#     print(f'Eu vou comer {comida}')
+# print('_'*30)
+# for cont in range(0, len(lanche)):
+#      print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+# print('_'*30)
+# for pos, comida in enumerate(lanche):
+#     print(f'Eu vou comer {comida} na posição {pos}')
+# print('Comi para caramba!')
