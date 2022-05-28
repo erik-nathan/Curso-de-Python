72c9830f5f78e0449520afb5716aa38b47aabfaf
erik-nathan <eriknathan.contato@gmail.com>
2022-05-28 19:42:07 -0300

    Adicioando o desafio 73

---
+++ PythonExercícios/ex079.py
@@ -0,0 +1,8 @@
+'''Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista.
+Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores
+únicos digitados, em ordem crescente..'''
+
+print("="*40)
+print("DESAFIO 78 - Valores únicos em uma Lista")
+print("="*40)
+
